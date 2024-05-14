28fedad380e1a2a17dfee82b7a6e10f2e1d1f629
PAMELA OLIVEIRA SANTOS <pamela.osantos3@senacsp.edu.br>
2024-05-13 22:00:01 -0300

    primeiro commit

--- app/routes.py
+++ app/routes.py
@@ -11,5 +11,22 @@ def index():
 def contato():
     return render_template('contato.html', titulo="Contato", nome="Pâmela")
 
+@app.route('/buque')
+def buque():
+    return render_template('buque.html', titulo="Buquê de Flores", nome="Pâmela")
+
+@app.route('/maisqueespeciais')
+def maisqueespeciais():
+    return render_template('maisqueespeciais', titulo="Mais que Especiais", nome="Pâmela")
+
+@app.route('/cestas')
+def cestas():
+    return render_template('cestas.html', titulo="Cestas", nome="Pâmela")
+
+@app.route('/encantadas')
+def encantadas():
+    return render_template('encantadas.html', titulo="Encantadas", nome="Pâmela")
+
+
 
 
